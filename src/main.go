bf113e0446a279d7480e4444ec06d4950a2b3b50
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-10-24 10:15:42 +0200

    refactor

--- src/main.go
+++ src/main.go
@@ -6,7 +6,14 @@ import (
 	"io/fs"
 	"log"
 	"os"
+
+	"github.com/a-h/templ"
 )
+
+type PageGenerator struct {
+	filename string
+	gen      func() templ.Component
+}
 
 var FLAGS = os.O_RDWR | os.O_CREATE
 var MODE fs.FileMode = 0644
@@ -15,59 +22,55 @@ func main() {
 	articleDir := os.Getenv("ARTICLE_DIR")
 	outputDir := os.Getenv("OUTPUT_DIR")
 
-	if articleDir == "" {
-		log.Fatal("ARTICLE_DIR is not set")
+	if articleDir == "" || outputDir == "" {
+		log.Fatal("ARTICLE_DIR and OUTPUT_DIR must be set")
 	}
 
-	if outputDir == "" {
-		log.Fatal("OUTPUT_DIR is not set")
-	}
+	workExperiences := buildWorkExperiences()
+	schoolExperiences := buildSchoolExperience()
 
 	articles, err := parseArticles(articleDir)
 	if err != nil {
 		log.Fatalf("Error while parsing articles: %v", err)
 	}
 
+	articleGenerators := make([]PageGenerator, 0)
+
 	for _, a := range articles {
-		filename := fmt.Sprintf("%s/%s", outputDir, a.HTMLFilename)
+		generator := func() templ.Component {
+			return article(a.Manifest.Title, a.Date, a.StringifiedHTML)
+		}
+
+		articleGenerators = append(articleGenerators, PageGenerator{
+			filename: a.HTMLFilename,
+			gen:      generator,
+		})
+	}
+
+	aboutGenerator := func() templ.Component {
+		return about(workExperiences, schoolExperiences)
+	}
+
+	resumeGenerator := func() templ.Component {
+		return resume(workExperiences, schoolExperiences)
+	}
+
+	pages := []PageGenerator{
+		{filename: "index.html", gen: home},
+		{filename: "about.html", gen: aboutGenerator},
+		{filename: "resume-light.html", gen: resumeGenerator},
+	}
+
+	pages = append(pages, articleGenerators...)
+
+	for _, page := range pages {
+		filename := fmt.Sprintf("%s/%s", outputDir, page.filename)
 		file, err := os.OpenFile(filename, FLAGS, MODE)
 		if err != nil {
 			log.Fatalf("Could not open file: %v", err)
 		}
 
-		aa := article(a.Manifest.Title, a.Date, a.StringifiedHTML)
-		aa.Render(context.Background(), file)
+		templComponent := page.gen()
+		templComponent.Render(context.Background(), file)
 	}
-
-	indexFilename := fmt.Sprintf("%s/index.html", outputDir)
-	indexFile, err := os.OpenFile(indexFilename, FLAGS, MODE)
-	if err != nil {
-		log.Fatalf("Could not open file: %v", err)
-	}
-
-	aboutFilename := fmt.Sprintf("%s/about.html", outputDir)
-	aboutFile, err := os.OpenFile(aboutFilename, FLAGS, MODE)
-	if err != nil {
-		log.Fatalf("Could not open file: %v", err)
-	}
-
-	resumeLightFilename := fmt.Sprintf("%s/resume-light.html", outputDir)
-	resumeLightFile, err := os.OpenFile(resumeLightFilename, FLAGS, MODE)
-	if err != nil {
-		log.Fatalf("Could not open file: %v", err)
-	}
-
-	home := home()
-	home.Render(context.Background(), indexFile)
-
-	workExperiences := buildWorkExperiences()
-	schoolExperiences := buildSchoolExperience()
-
-	about := about(workExperiences, schoolExperiences)
-	about.Render(context.Background(), aboutFile)
-
-	// Print resume to a standalone HTMl file so that we can easily create a PDF from it
-	// Will most likely be removed before serving the content
-	resumeLight := resumeLight(workExperiences, schoolExperiences)
-	resumeLight.Render(context.Background(), resumeLightFile)
 }
